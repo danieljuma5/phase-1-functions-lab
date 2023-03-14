746dd4a4f5512d8ff07f068e5aee9bf50688e5e1
danieljuma5 <jumadaniel275@gmail.com>
2023-03-14 15:38:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+// Solution to Scuber commands
+
+function distanceFromHqInBlocks(block) {
+  if (block >= 42) {
+    return block - 42;
+  } else if (block < 42) {
+    return 42 - block;
+  }
+
+}
+
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(a,b) {
+  if (b >= a) {
+    return (b - a) * 264;
+  } else if (b < a) {
+    return (a-b) * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  let mileAge = distanceTravelledInFeet(start,destination);
+  if (mileAge <= 400) {
+    return 0;
+  }else if (mileAge >= 400 && mileAge <= 2000 ) {
+    return (mileAge - 400) * 0.02;
+  } else if (mileAge >= 2000 && mileAge <= 2500) {
+    return 25;
+  } else if (mileAge >2500) {
+    return "cannot travel that far"
+
+  }
+}
